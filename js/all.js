111a1d0395976dcdbedc3fd88b19dedc4cf823bb
5eancolor <seancolor@gmail.com>
2026-09-02 14:08:15 +0800

    Home work done

--- js/all.js
+++ js/all.js
@@ -1,9 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.dropdown').click(function(event) {
-    /* Act on the event */
-    event.preventDefault();
-    $('.dropdown').toggleClass('active');
-    $('.dropdown-open').slideToggle();
-  });
+//下拉式選單
+    $(".menu>ul>li").hover(function(){
+        $(this).find(".dropdown-open").stop().slideToggle();
+        $(this).find("dropdown").toggleClass("active");
+    });
+
+//banner swiper
+   const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+  // If we need pagination
+  //pagination: {
+    //el: '.swiper-pagination',
+  //},
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+//scrolltop
+
+    $(".arrow a").click(function(event){
+        event.preventDefault();
+        $("html,body").animate({
+            scrollTop: 0
+        }, 700);
+    });
+
 });
